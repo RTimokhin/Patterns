c57fde21c042513881bc4452771808d98086ffe8
Roman <roman.timokhin88@ya.ru>
2020-01-30 14:22:40 +0300

    Patterns

--- Facade.js
+++ Facade.js
@@ -1,41 +1,28 @@
-//паттерн фасад - создаёт более простой интерфейс для взаимодействия с классами
-class Complaints {
-  constructor() {
-    this.complaints = []; //инициализируем пустой массив
-  }
-  reply(complaints) {}; //инициализируем метод reply
-  add(complaint) {
-    this.complaints.push(complaint); //добавляет элемент в массив complaints
-    return this.reply(complaint); //возвращает результат работы метода reply
-  }
+//паттерн фасад - позволяет скрыть сложность системы путём сведения всех возможных вызовов к одному объекту, делегирующему их соответствующих объектам системы
+const module = (function() {
+const _private = {
+i: 5,
+get: function() {
+console.log('Текущее значение:' + this.i);
+},
+set: function(val) {
+this.i = val;
+},
+run: function() {
+console.log('процесс запущен');
+},
+jump: function() {
+console.log('резкое изменение');
 }
-
-class ProductComplaints extends Complaints { //класс наследуется от класса Complaints
-  reply({id, customer, details}) { //создадим функциональность метода reply для класса ProductComplaints
-    return `Product: ${id}: ${customer} (${details})`
-  }
+};
+return {
+facade: function(args) {
+_private.set(args.val);
+_private.get();
+if (args.run) {
+_private.run();
 }
-
-class ServiceComplaints extends Complaints { //класс наследуется от класса Complaints
-  reply({id, customer, details}) { //создадим функциональность метода reply для класса ServiceComplaints
-    return `Service: ${id}: ${customer} (${details})`
-  }
 }
-
-class ComplaintRegistry { //создадим класс, к-ый по сути и будет являться шаблоном фасад
-  register(customer, type, details) {
-    //расширим класс дополнительными полями
-    const id = Date.now();
-    let complaint;
-    if(type === 'service') {
-      complaint = new ServiceComplaints();
-    } else {
-      complaint = new ProductComplaints();
-    }
-    return complaint.add({id, customer, details}); //вернём объект complaint
-  }
 }
-
-const registry = new ComplaintRegistry();
-console.log(registry.register('Roman', 'service', 'undefined'));
-console.log(registry.register('Elena', 'product', 'error'));
+}());
+module.facade({run:true, val:10});
